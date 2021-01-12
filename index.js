c9bbd3c4386e6c95ad01f414ccc45edaaee6147a
Tony <thanh.nv@appman.co.th>
2021-01-12 17:13:55 +0700

    setup

--- index.js
+++ index.js
@@ -12,11 +12,11 @@ if (!databaseUri) {
 }
 
 var api = new ParseServer({
-  databaseURI: databaseUri || 'mongodb+srv://poc-tony:abc123456@cluster0.xkgj8.mongodb.net/poc?retryWrites=true&w=majority',
+  databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
   appId: process.env.APP_ID || 'poc-tony',
   masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
-  serverURL: process.env.SERVER_URL || 'https://poc-tony.herokuapp.com/',  // Don't forget to change to https if needed
+  serverURL: process.env.SERVER_URL || 'https://poc-tony.herokuapp.com/parse/',  // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
   }
