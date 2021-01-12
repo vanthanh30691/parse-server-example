286349474f5d9addd9bcab98baae2cc69a31aaae
Tony <thanh.nv@appman.co.th>
2021-01-12 16:58:40 +0700

    add db

--- index.js
+++ index.js
@@ -12,9 +12,9 @@ if (!databaseUri) {
 }
 
 var api = new ParseServer({
-  databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
+  databaseURI: databaseUri || 'mongodb+srv://poc-tony:abc123456@cluster0.xkgj8.mongodb.net/poc?retryWrites=true&w=majority',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || 'myAppId',
+  appId: process.env.APP_ID || 'poc-tony',
   masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
   liveQuery: {
